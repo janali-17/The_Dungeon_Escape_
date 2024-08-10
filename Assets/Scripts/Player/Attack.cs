7592d82a260c6f3c83de9818cf3907a5c9d6c86e
Jan Ali <68240754+janali-17@users.noreply.github.com>
2024-08-10 12:53:36 -0700

    added layers and collider to sword and skeleton

---
+++ Assets/Scripts/Player/Attack.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Attacking");
+        Debug.Log("Hit :" + other.name);
+    }
+
+}
