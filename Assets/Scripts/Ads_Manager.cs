8137e6d001ec8ab721f3d495b6df161ff362f0fc
Jan Ali <68240754+janali-17@users.noreply.github.com>
2024-08-26 12:13:12 -0700

    Ads_Script code(error)

---
+++ Assets/Scripts/Ads_Manager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class RewardedAdDisplay : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
+{
+    public string myGameIdAndroid = "YOUR_GAME_ID_HERE";
+    public string myGameIdIOS = "YOUR_GAME_ID_HERE";
+    public string adUnitIdAndroid = "Rewarded_Android";
+    public string adUnitIdIOS = "Rewarded_iOS";
+    public string myAdUnitId;
+    public string myAdStatus = "";
+    public bool adStarted;
+    public bool adCompleted;
+
+    private bool testMode = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+#if UNITY_IOS
+        Advertisement.Initialize(myGameIdIOS, testMode, this);
+        myAdUnitId = adUnitIdIOS;
+#else
+        Advertisement.Initialize(myGameIdAndroid, testMode, this);
+        myAdUnitId = adUnitIdAndroid;
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialization complete.");
+        Advertisement.Load(myAdUnitId, this);
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        myAdStatus = message;
+        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+    }
+
+    public void OnUnityAdsAdLoaded(string adUnitId)
+    {
+        Debug.Log("Ad Loaded: " + adUnitId);
+        if (!adStarted)
+        {
+            Advertisement.Show(myAdUnitId, this);
+        }
+    }
+    public void OnUnityAdsFailedToLoad(string adUnitId, UnityAdsLoadError error, string message)
+    {
+        myAdStatus = message;
+        Debug.Log($"Error showing Ad Unit {adUnitId}: {error.ToString()} - {message}");
+
+    }
+
+    public void OnUnityAdsShowFailure(string adUnitId, UnityAdsShowError error, string message)
+    {
+        myAdStatus = message;
+        Debug.Log($"Error showing Ad Unit {adUnitId}: {error.ToString()} - {message}");
+    }
+
+    public void OnUnityAdsShowStart(string adUnitId)
+    {
+        adStarted = true;
+        Debug.Log("Ad Started: " + adUnitId);
+    }
+
+    public void OnUnityAdsShowClick(string adUnitId)
+    {
+        Debug.Log("Ad Clicked: " + adUnitId);
+    }
+
+    public void OnUnityAdsShowComplete(string adUnitId, UnityAdsShowCompletionState showCompletionState)
+    {
+        adCompleted = showCompletionState == UnityAdsShowCompletionState.COMPLETED;
+        Debug.Log("Ad Completed: " + adUnitId);
+    }
+} // <--- This bracket was missing
