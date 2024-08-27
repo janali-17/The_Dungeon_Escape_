96020f13e7c74669d889fb3518c93179f82efb07
Jan Ali <68240754+janali-17@users.noreply.github.com>
2024-08-27 15:32:25 -0700

    Game Fully Completed
    I have Added some Extra Stuff like end level animation and pause panel and fix some bugs too

---
+++ Assets/Scripts/Level_End_Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_End_Script : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _EndEvelAnimator;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && GameManager.Instance.HasKey == true)
+        {
+             _EndEvelAnimator.updateMode = AnimatorUpdateMode.UnscaledTime;
+            Debug.Log("Player collide with end level");
+             Time.timeScale = 0.0f;
+            _EndEvelAnimator.SetTrigger("GameOver");
+        }
+    }
+}
