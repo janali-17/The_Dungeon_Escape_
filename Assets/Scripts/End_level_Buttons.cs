2e6d492de45af8289b13ad9a3a835068ca83fbb7
Jan Ali <68240754+janali-17@users.noreply.github.com>
2024-08-27 16:14:36 -0700

    Revert "Completed Final touch"
    This reverts commit f4cdb6c0fb7174999aae96488b84a64306138428.

---
+++ Assets/Scripts/End_level_Buttons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End_level_Buttons : MonoBehaviour
+{
+    public void Menu_Button()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
